f2ea9e2d0e04c5732b88d18e22ad41ba671428ba
Gabriel Viccari <gabriel.viccari20@gmail.com>
2024-03-07 11:52:49 -0300

    correction to last commit, and added findAll mock

--- rest-api-with-spring-boot/src/main/java/com/gabriel/services/PersonServices.java
+++ rest-api-with-spring-boot/src/main/java/com/gabriel/services/PersonServices.java
@@ -3,6 +3,8 @@ package com.gabriel.services;
 import com.gabriel.models.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -10,6 +12,16 @@ import java.util.logging.Logger;
 public class PersonServices {
     private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public List<Person> findAll() {
+        List<Person> persons = new ArrayList<>();
+        logger.info("Getting all persons");
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
 
     public Person findById(String id) {
         logger.info("finding one person by id");
@@ -22,4 +34,15 @@ public class PersonServices {
         person.setGender("Male");
         return person;
     }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setName("person name " + i);
+        person.setLastName("person lastName " + i);
+        person.setAddress("person address " + i);
+        person.setGender("person gender " + i);
+        return person;
+    }
+
 }
