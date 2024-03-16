58beb475492a50b8c645d630803621c208845642
Gabriel Viccari <gabriel.viccari20@gmail.com>
2024-03-15 22:21:00 -0300

    Content Negotiation, content data in json and xml

--- rest-api-with-spring-boot/src/main/java/com/gabriel/data/vo/v1/PersonVO.java
+++ rest-api-with-spring-boot/src/main/java/com/gabriel/data/vo/v1/PersonVO.java
@@ -1,11 +1,14 @@
 package com.gabriel.data.vo.v1;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
-
+@JsonPropertyOrder({"id", "first_name", "last_name", "address", "gender"})
 public class PersonVO implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
@@ -13,8 +16,9 @@ public class PersonVO implements Serializable {
 
     private Long id;
 
+    @JsonProperty("first_name")
     private String firstName;
-
+    @JsonProperty("last_name")
     private String lastName;
 
     private String address;
