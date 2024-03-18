47c452e23d9723a84141621d7af6dc3396e11be2
Gabriel Viccari <gabriel.viccari20@gmail.com>
2024-03-18 00:56:49 -0300

    content negotiation via Header Param

--- rest-api-with-spring-boot/src/main/java/com/gabriel/config/WebConfig.java
+++ rest-api-with-spring-boot/src/main/java/com/gabriel/config/WebConfig.java
@@ -7,13 +7,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        /*Configurer via queryParam http://localhost:8080/api/person/v1?mediaType=xml
         configurer.favorParameter(true);
         configurer.parameterName("mediaType");
         configurer.ignoreAcceptHeader(true).useRegisteredExtensionsOnly(false);
         configurer.defaultContentType(MediaType.APPLICATION_JSON)
-                .mediaType("json",MediaType.APPLICATION_JSON)
-                .mediaType("xml",MediaType.APPLICATION_XML);
+        .mediaType("json",MediaType.APPLICATION_JSON)
+        .mediaType("xml",MediaType.APPLICATION_XML); */
 
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
 
     }
 }
